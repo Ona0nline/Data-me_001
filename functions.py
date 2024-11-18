5f3c816a449a1c5c116ac5c7b8cbb60c3cbc44bd
Onalerona <onzwanejhb024@student.wethinkcode.co.za>
2024-11-18 11:13:06 +0200

    Test time up

--- functions.py
+++ functions.py
@@ -1,12 +1,22 @@
 # TODO: Implement the following functions based on the descriptions.
 
+element = 2
+dct= {'a': 1, 'b': 2, 'c': 1}
+value = 1
+
+
+
 def reverse_list(lst):
     """
     Reverses the given list.
     :param lst: List of integers.
     :return: A list with elements in reverse order.
     """
-    pass  # Implement this
+    lst = [1,2,3]
+    print(lst.reverse())
+    return lst.reverse()
+    
+  
 
 def count_occurrences(lst, element):
     """
@@ -15,7 +25,15 @@ def count_occurrences(lst, element):
     :param element: Element to count.
     :return: Integer count of occurrences.
     """
-    pass  # Implement this
+    
+    element_count = 0
+    for item in lst:
+        if element == item:
+            element_count += 1
+            print(f"Element occurs {element_count} times")
+
+    return element_count
+    
 
 def get_keys_with_value(dct, value):
     """
@@ -24,7 +42,14 @@ def get_keys_with_value(dct, value):
     :param value: Value to find.
     :return: List of keys.
     """
-    pass  # Implement this
+    list = []
+    for key in dct:
+        if value == dct[key]:
+            list.append(key)
+        
+    print(list)
+    return list
+    
 
 def merge_sorted_lists(lst1, lst2):
     """
@@ -33,7 +58,16 @@ def merge_sorted_lists(lst1, lst2):
     :param lst2: Second sorted list.
     :return: Merged sorted list.
     """
-    pass  # Implement this
+    # lst1 = [1, 3, 5]
+    # lst2 = [2, 4, 6]
+    
+    # sort1 = lst1.sort()
+    # sort2 = lst2.sort()
+    # merged = sort1 + sort2
+    # # print(f"{sort1} + {sort2}")
+    # return merged
+    pass
+
 
 def find_second_largest(numbers):
     """
@@ -55,7 +89,17 @@ def is_anagram(str1, str2):
     :param str2: Second string.
     :return: True if the strings are anagrams, False otherwise.
     """
-    pass  # Implement this
+    str1 = "hello"
+    str2 = "world"
+    
+
+    if list(str1).sort() == list(str2).sort():
+        print('True')
+        return True
+    else:
+        print('False')
+        return False
+    
 
 
 def flatten_list(nested_list):
@@ -64,7 +108,9 @@ def flatten_list(nested_list):
     :param nested_list: List of lists.
     :return: A flat list with all elements.
     """
-    pass  # Implement this
+    # nested_list = [[1, 2], [3, 4]]
+    flattened = list(zip(*nested_list))
+    print(flattened)
 
 
 def remove_duplicates(lst):
@@ -73,7 +119,19 @@ def remove_duplicates(lst):
     :param lst: List of elements.
     :return: List without duplicates.
     """
-    pass  # Implement this
+    lst = [1, 2, 2, 3, 3]
+    count = 0 
+    print(lst)
+    for item in lst:
+        if item:
+            count += 1
+
+            if count > 1:
+                print(item)
+                lst.remove(item)
+    print(lst)
+    return lst
+
 
 def find_common_elements(lst1, lst2):
     """
@@ -82,4 +140,30 @@ def find_common_elements(lst1, lst2):
     :param lst2: Second list.
     :return: List of common elements.
     """
-    pass  # Implement this
+    lst1 = [1, 2, 3] 
+    lst2 = [3, 4, 5]
+
+    for i in lst1:
+        for j in lst2:
+            if i in lst2 and j in lst1:
+                print(i)
+    return i
+lst = [1, 2, 3]
+reverse_list(lst)
+# element = input("Element:")
+count_occurrences(lst, element)
+get_keys_with_value(dct, value)
+
+lst1 = [1, 3, 5]
+lst2 = [2, 4, 6]
+merge_sorted_lists(lst1, lst2)
+
+str1 = "hello"
+str2 = "world"
+is_anagram(str1, str2)
+find_common_elements(lst1, lst2)
+nested_list = [[1, 2], [3, 4]]
+flatten_list(nested_list)
+
+lst = [1, 2, 2, 3, 3]
+remove_duplicates(lst)
